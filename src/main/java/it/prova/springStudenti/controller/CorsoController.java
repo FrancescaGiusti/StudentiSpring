8b310fd7b3d1f573a56a3345dc9993a93cd4636b
FrancescaGiusti <francesca.pescia01@gmail.com>
2025-12-22 10:04:34 +0100

    "Eliminati import superflui"

--- src/main/java/it/prova/springStudenti/controller/CorsoController.java
+++ src/main/java/it/prova/springStudenti/controller/CorsoController.java
@@ -5,9 +5,7 @@ import it.prova.springStudenti.model.Corso;
 import it.prova.springStudenti.service.CorsoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
